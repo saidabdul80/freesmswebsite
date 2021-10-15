9627c29de2e12d321d1f64373fd888b4f6fe14c8
Said Abdulsalam <saidabdulsalam5@gmail.com>
2021-10-15 13:30:41 +0100

    fix334

--- about.php
+++ about.php
@@ -26,7 +26,7 @@ include('header.php');
       </div>
     </section><!-- End About Us Section -->
 
-    <!-- ======= Our Team Section ======= -->
+   
     <section id="team" class="team section-bg">
       <div class="container">
 
@@ -40,17 +40,17 @@ include('header.php');
           <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
             <div class="member" data-aos="fade-up">
               <div class="member-img">
-                <img src="assets/img/team-2.jpg" class="img-fluid" alt="">
+                <img src="assets/img/team-1.jpg" class="img-fluid" alt="">
                 <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
                   <a href=""><i class="bi bi-facebook"></i></a>
                   <a href=""><i class="bi bi-instagram"></i></a>
                   <a href=""><i class="bi bi-linkedin"></i></a>
                 </div>
-              </div>
+              </div> 
               <div class="member-info">
-                <h4>Jamiu </h4>
-                <span>Chief Executive Officer</span>
+                <h4>Jamiu yusuf </h4>
+                <span>Chief Executive Officer </span>
               </div>
             </div>
           </div>
