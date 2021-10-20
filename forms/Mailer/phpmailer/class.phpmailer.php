72f2ba56865f204f4c4c3f00d227a1a4af1adb43
Said Abdulsalam <saidabdulsalam5@gmail.com>
2021-10-20 09:49:36 +0100

    fix

--- forms/Mailer/phpmailer/class.phpmailer.php
+++ forms/Mailer/phpmailer/class.phpmailer.php
@@ -2216,7 +2216,9 @@ class PHPMailer
             if (!is_readable($path)) {
                 throw new phpmailerException($this->lang('file_open') . $path, self::STOP_CONTINUE);
             }
-            $magic_quotes = get_magic_quotes_runtime();
+            
+            //$magic_quotes = get_magic_quotes_runtime();
+            $magic_quotes = false; 
             if ($magic_quotes) {
                 if (version_compare(PHP_VERSION, '5.3.0', '<')) {
                     set_magic_quotes_runtime(0);
